e9796e3cfc2de91c1ce95c8e48bf2b2403628fcb
Yuzhe <yuzhelu@gmail.com>
2018-04-19 19:09:44 -0600

    hw5 updates on server.js

--- server.js
+++ server.js
@@ -19,11 +19,21 @@ mongoose.connect(process.env.mongoDB, (err, database) =>{
 
 })
 
+
+
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 var router = express.Router();
 
