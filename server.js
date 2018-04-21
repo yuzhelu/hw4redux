430d563e8f101a4851958b60dfdacbfa7532f5bf
Yuzhe <yuzhelu@gmail.com>
2018-04-21 15:12:53 -0600

    hw5 updates on server.js

--- server.js
+++ server.js
@@ -103,8 +103,31 @@ router.route('/movies')
     .get(authJwtController.isAuthenticated, function (req, res) {
         Movie.find(function (err, movies) {
             if (err) res.send(err);
-            // return the users
-            res.json(movies);
+            //movies have been found
+            var reviewOption = req.query.reviews;
+
+            if(reviewOption === "true"){
+                Movie.aggregate([{
+                    $lookup: {
+                        from: "reviews",
+                        localField: "title",
+                        foreignField: "movieTitle",
+                        as: 'review'
+                    }
+                },
+
+                    {
+                        $match:{ title: movie.title }
+                    }
+
+                ], function (err, result) {
+                    if(err) res.send(err);
+                    else res.json(result);
+                });
+            } else {
+                res.json(movie);
+            }
+
         });
     })
 
