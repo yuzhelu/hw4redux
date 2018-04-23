e4d87d68720284302fee742ad0bf7f85293c09ed
Yuzhe Lu <yuzhelu@gmail.com>
2018-04-23 17:45:32 -0600

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(cors());
+app.use(function (req, res, next) {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+res.setHeader('Access-Control-Allow-Credentials', true);
+next();
+});
 
 var router = express.Router();
 
